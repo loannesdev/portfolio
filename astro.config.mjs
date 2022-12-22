47db27f7bc0f4b4b7e7cee764bbfbd37eabc0623
Loannes <44909579+LoannesSecat@users.noreply.github.com>
2022-12-22 18:38:25 -0500

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import tokencss from "@tokencss/astro";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tokencss()]
+});
