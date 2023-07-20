a0cb124d50a99c3a35728f240e845228e4ce1637
Loannes <44909579+LoannesSecat@users.noreply.github.com>
2023-07-20 18:54:00 -0500

    refactor: projects filter

--- src/services/github.js
+++ src/services/github.js
@@ -1,18 +1,24 @@
-import { github } from "../utils/info";
+import { github } from "../utils/const";
 
 const token = import.meta.env.PUBLIC_GITHUB_TOKEN;
 const { api, path, myUser } = github;
-const myHeaders = {
-  headers: {
-    "Authorization": `Bearer ${token}`,
-    "X-GitHub-Api-Version": "2022-11-28"
-  }
+const headers = {
+  "Authorization": `Bearer ${token}`,
+  "X-GitHub-Api-Version": "2022-11-28",
+  "Accept": "application/vnd.github+json"
 }
 
+const dontShow = ["portfolio", "manfred", "loannesdev"];
+
 export const readRepos = async () => {
+  const url = `${api}${path.users}/${myUser}${path.repos}`
+
   try {
-    const res = await fetch(`${api}${path.users}/${myUser}${path.repos}`, { ...myHeaders })
-    return res.json();
+    const res = await fetch(url, { headers })
+    const json = await res.json();
+    const filteredData = json.filter(repo => !repo.fork && !dontShow.some((name) => name === repo.name))
+
+    return filteredData
   }
 
   catch {
