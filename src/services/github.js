7675615237bd4cb66c1d5a312128f6a95af7d12c
Loannes <44909579+LoannesSecat@users.noreply.github.com>
2023-07-26 22:28:23 -0500

    refactor: svelte added & some fixes

--- src/services/github.js
+++ src/services/github.js
@@ -31,8 +31,9 @@ export const readResume = async () => {
   try {
     const res = await fetch(url, { headers });
     const { content } = await res.json();
+
     const base64Decode = atob(content);
-    const utf8Encode = Buffer.from(base64Decode, "latin1").toString("utf8");
+    const utf8Encode = new TextDecoder("utf8").decode(new Uint8Array([...base64Decode].map((c) => c.charCodeAt(0))));
     const data = JSON.parse(utf8Encode);
 
     return data;
