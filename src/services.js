799b7960ad3637dbfee33cfcc54da4b6077af411
loannesdev <zsevaxt@gmail.com>
2025-05-28 23:15:34 -0500

    refactor: hide portfolio proyect

--- src/services.js
+++ src/services.js
@@ -9,7 +9,7 @@ const headers = {
   Accept: "application/vnd.github+json"
 };
 
-const dontShow = ["manfred", "loannesdev", "to-do-list", "console-js"];
+const dontShow = ["manfred", "loannesdev", "to-do-list", "console-js", "portfolio"];
 
 export const readRepos = async () => {
   const url = `${api}${path.users}/${user}${path.repos}?type=public`;
